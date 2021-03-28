b42c45896bc076ce7979a7f0ed771e6d564bdc6f
LucRiviere <75257840+LucRiviere@users.noreply.github.com>
2021-03-28 19:58:42 +0200

    Add files via upload

---
+++ CDate.h
@@ -0,0 +1,40 @@
+#ifndef CDATE_H
+#define CDATE_H
+#include <string>
+
+using namespace std;
+
+
+
+class CDate
+{
+    public:
+
+    CDate(int annee, int mois, int jour, int heure, int minute, int seconde, string nom_jour);
+
+    void getDate(int& a, int& m, int& j, int& h, int& mn, int& s, string& Njour);
+
+
+    bool operator > (CDate d);
+
+    bool operator < (CDate d);
+
+    bool operator == (CDate d);
+
+
+    virtual ~CDate();
+
+
+
+    private:
+
+        int annee;
+        int mois;
+        int jour;
+        int heure;
+        int minute;
+        int seconde;
+        string nom_jour;
+};
+
+#endif // CDATE_H
