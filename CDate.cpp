d7b0c70b4b4f6582fc32e91d7e06e1aa4939cdf7
LucRiviere <75257840+LucRiviere@users.noreply.github.com>
2021-03-29 22:16:53 +0200

    Update CDate.cpp

--- CDate.cpp
+++ CDate.cpp
@@ -1,7 +1,22 @@
+/*************************************************/
+// Nom du projet: Date
+// Nom du fichier: CDate.cpp
+// Version : 1.0
+// Nom du programmeur: Luc Rivière
+// Date de création : 29/03/2021
+// Rôle du fichier: Contient le code des méthodes de la date
+// Nom des composants utilises: CDate.h
+//                              string 
+// Historique du fichier:
+/*************************************************/
 #include "CDate.h"
+#include <iostream>
 #include <string>
-
 using namespace std;
+
+    // Nom :CDate  Constructeur
+    // Rôle : Initialise la date
+    // Sortie : La date créée 
 
 CDate::CDate(int annee, int mois, int jour, int heure, int minute, int seconde, string nom_jour)
 {
@@ -11,38 +26,38 @@ CDate::CDate(int annee, int mois, int jour, int heure, int minute, int seconde, 
     this->heure = heure;
     this->minute = minute;
     this->seconde = seconde;
-    this-> nom_jour = nom_jour;
+    this->nom_jour = nom_jour;
 }
 
-bool CDate::operator > ( CDate d)
+    // Nom : getDate
+    // Rôle : indique la date en appelant la méthode, c'est un accesseur
+    // Paramètre d'entrée : la date appelant la méthode
+    // Sortie : l'année, le mois, le jour, le heure, la minute, la seconde, le nom du jour de la date
 
+void CDate::getDate(int& a, int& m, int& j, int& h, int& mn, int& sec, string& NJour)
 {
-        return (this->annee > d.annee && this->mois > d.mois && this->jour >d.jour && this->heure > d.heure && this->minute >d.minute && this ->seconde > d.seconde && this-> nom_jour > d.nom_jour);
+    a= this->annee;
+    m=this->mois;
+    j= this->jour;
+    h= this->heure;
+    mn= this->minute;
+    sec= this->seconde;
+    NJour=this->nom_jour;
 }
+    // Nom : operator
+    // Rôle : Permet de comparer deux dates pour savoir si elles sont égales ou de savoir si une date est postérieure ou antérieure à une autre,
+    // Paramètre d'entrée : La date appelant la méthode et la date dont on fait la comparaison
+    // Sortie : C'est un booléen qui indique si les deux dates sont inférieures ou supérieures 
 
-bool CDate::operator < ( CDate d)
-
-{
-        return (this->annee < d.annee && this->mois < d.mois && this->mois == d.mois && this->jour < d.jour && this->heure < d.heure && this->minute < d.minute && this ->seconde < d.seconde && this-> nom_jour < d.nom_jour);
-}
-
-bool CDate::operator == (CDate d)
-
-{
+    bool CDate::operator > (const CDate d)
+    {
+        return (this->annee > d.annee);
+    }
+    bool CDate::operator < (const CDate d)
+    {
+        return (this->annee < d.annee);
+    }
+    bool CDate::operator == (const CDate d)
+    {
         return (this->annee == d.annee && this->mois == d.mois && this->jour == d.jour && this->heure == d.heure && this->minute == d.minute && this->seconde == d.seconde && this->nom_jour == d.nom_jour);
-}
-
-void CDate::getDate(int& a, int& m, int& j, int& h, int& mn, int& sec, string& Njour)
-
-{
-    this->annee = a;
-    this->mois = m;
-    this->jour = j;
-    this->heure = h;
-    this->minute = mn;
-    this->seconde = sec;
-    this ->nom_jour = Njour;
-
-}
-
-
+    }
